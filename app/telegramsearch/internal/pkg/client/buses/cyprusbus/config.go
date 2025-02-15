914e4513f730950577eb78285f0ad21f04c2457a
Anton Chaporgin <chapson@neon.tech>
2025-02-15 17:52:39 +0200

    do not expose baseURL

--- app/telegramsearch/internal/pkg/client/buses/cyprusbus/config.go
+++ app/telegramsearch/internal/pkg/client/buses/cyprusbus/config.go
@@ -1,28 +1,29 @@
 package cyprusbus
 
 import (
-	"time"
+  "time"
 
-	"github.com/yanakipre/bot/internal/encodingtooling"
-	"github.com/yanakipre/bot/internal/resttooling"
+  "github.com/yanakipre/bot/internal/encodingtooling"
+  "github.com/yanakipre/bot/internal/resttooling"
 )
 
 type Config struct {
-	Timer         encodingtooling.Duration
-	BoxSizeMeters float64
-	BaseURL       string
-	HTTPTransport resttooling.Config
+  Timer         encodingtooling.Duration
+  BoxSizeMeters float64
+  BaseURL       string
+  HTTPTransport resttooling.Config
 }
 
-const BaseUrl string = "http://20.19.98.194:8328/Api"
+// this URL is retrieved from data.gov.cy.
+const baseUrl string = "http://20.19.98.194:8328/Api"
 
 func DefaultConfig() Config {
-	tr := resttooling.DefaultTransportConfig()
-	tr.ClientName = "cyprusbus"
-	return Config{
-		Timer:         encodingtooling.NewDuration(20 * time.Second),
-		BoxSizeMeters: 1000,
-		BaseURL:       BaseUrl,
-		HTTPTransport: tr,
-	}
+  tr := resttooling.DefaultTransportConfig()
+  tr.ClientName = "cyprusbus"
+  return Config{
+    Timer:         encodingtooling.NewDuration(20 * time.Second),
+    BoxSizeMeters: 1000,
+    BaseURL:       baseUrl,
+    HTTPTransport: tr,
+  }
 }
