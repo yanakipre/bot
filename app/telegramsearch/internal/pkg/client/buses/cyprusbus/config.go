98bca23464f2e49ee904554fe4a51aba3668032b
Anton Chaporgin <chapson@neon.tech>
2025-02-15 17:53:27 +0200

    added yaml into config structs

--- app/telegramsearch/internal/pkg/client/buses/cyprusbus/config.go
+++ app/telegramsearch/internal/pkg/client/buses/cyprusbus/config.go
@@ -1,29 +1,29 @@
 package cyprusbus
 
 import (
-  "time"
+	"time"
 
-  "github.com/yanakipre/bot/internal/encodingtooling"
-  "github.com/yanakipre/bot/internal/resttooling"
+	"github.com/yanakipre/bot/internal/encodingtooling"
+	"github.com/yanakipre/bot/internal/resttooling"
 )
 
 type Config struct {
-  Timer         encodingtooling.Duration
-  BoxSizeMeters float64
-  BaseURL       string
-  HTTPTransport resttooling.Config
+	Timer         encodingtooling.Duration `yaml:"timer"`
+	BoxSizeMeters float64                  `yaml:"box_size_meters"`
+	BaseURL       string                   `yaml:"base_url"`
+	HTTPTransport resttooling.Config       `yaml:"http_transport"`
 }
 
 // this URL is retrieved from data.gov.cy.
 const baseUrl string = "http://20.19.98.194:8328/Api"
 
 func DefaultConfig() Config {
-  tr := resttooling.DefaultTransportConfig()
-  tr.ClientName = "cyprusbus"
-  return Config{
-    Timer:         encodingtooling.NewDuration(20 * time.Second),
-    BoxSizeMeters: 1000,
-    BaseURL:       baseUrl,
-    HTTPTransport: tr,
-  }
+	tr := resttooling.DefaultTransportConfig()
+	tr.ClientName = "cyprusbus"
+	return Config{
+		Timer:         encodingtooling.NewDuration(20 * time.Second),
+		BoxSizeMeters: 1000,
+		BaseURL:       baseUrl,
+		HTTPTransport: tr,
+	}
 }
