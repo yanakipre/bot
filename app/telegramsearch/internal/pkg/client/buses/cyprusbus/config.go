a38c4df36f3f37883aa2fb1c5d2fbd8d519400cf
Anton Chaporgin <chapson@neon.tech>
2025-02-15 17:55:37 +0200

    more docs

--- app/telegramsearch/internal/pkg/client/buses/cyprusbus/config.go
+++ app/telegramsearch/internal/pkg/client/buses/cyprusbus/config.go
@@ -10,10 +10,12 @@ import (
 type Config struct {
 	// Timer is the time interval between 2 requests to the API.
 	// It is used to understand the directions of the buses.
-	Timer         encodingtooling.Duration `yaml:"timer"`
-	BoxSizeMeters float64                  `yaml:"box_size_meters"`
-	BaseURL       string                   `yaml:"base_url"`
-	HTTPTransport resttooling.Config       `yaml:"http_transport"`
+	Timer encodingtooling.Duration `yaml:"timer"`
+	// BoxSizeMeters limits the size of the bounding box around the user's location.
+	// Buses outside of this box are not considered.
+	BoxSizeMeters float64            `yaml:"box_size_meters"`
+	BaseURL       string             `yaml:"base_url"`
+	HTTPTransport resttooling.Config `yaml:"http_transport"`
 }
 
 // this URL is retrieved from data.gov.cy.
